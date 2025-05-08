5befc83d94c40f62800f8ed7ab6ee8312a477f85
dsmarcelo <celoygm@gmail.com>
2025-05-08 08:43:37 -0300

    feat: add color indication for positive/negative profit values

--- src/app/_components/profit-text.tsx
+++ src/app/_components/profit-text.tsx
@@ -14,7 +14,11 @@ export default async function ProfitText({
     <div className="flex flex-col gap-4">
       <h5 className="text-xl font-bold">Lucro</h5>
       {/* Optionally, you can show a placeholder or skeleton here if params are missing */}
-      <h1 className="text-4xl font-bold">{formatCurrency(profit)}</h1>
+      <h1
+        className={`text-4xl font-bold ${profit >= 0 ? "text-blue-900" : "text-red-700"}`}
+      >
+        {formatCurrency(profit)}
+      </h1>
     </div>
   );
 }
