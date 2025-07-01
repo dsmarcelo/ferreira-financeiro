5c9c4b078fcdd862b9c73ddb0775b1c00c26f515
dsmarcelo <celoygm@gmail.com>
2025-06-30 22:52:13 -0300

    chore: update favicon and app icons, add new logo-financeiro image, and modify layout title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
 import "@/styles/globals.css";
 
 import { type Metadata } from "next";
-// import { Geist } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import BottomNav from "@/app/_components/bottom-nav";
 
 export const metadata: Metadata = {
-  title: "Prime Controle Financeiro",
+  title: "Ferreiro Controle Financeiro",
   description: "Controle Financeiro",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
-
-// const geist = Geist({
-//   subsets: ["latin"],
-//   variable: "--font-geist-sans",
-// });
 
 export default function RootLayout({
   children,
@@ -22,7 +16,6 @@ export default function RootLayout({
   return (
     <html
       lang="pt-BR"
-      // className={`${geist.variable}`}
     >
       <body>
         <main className="mx-auto pb-32">{children}</main>
